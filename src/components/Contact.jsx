931bac1583f992d595fe42bbb9e9a7ae2ff5b1d2
Dridox <bastien.heitzmann@gmail.com>
2024-08-31 16:39:06 +0200

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
           <div className="square" />
         </span>
         <p className="title">Voici le CV</p>
-        <a className="title" href="src\constants\Heitzmann_BASTIEN.pdf" download="Heitzmann_BASTIEN.pdf"> Ouvrir </a>
+        <a className="title" href="public\Heitzmann_BASTIEN.pdf" download="Heitzmann_BASTIEN.pdf"> Ouvrir </a>
       </label>
     </div>
   </motion.p>
