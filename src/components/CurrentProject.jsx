38cbe995b5d729995f4b7e142b373cdfbda28ed2
Dridox <bastien.heitzmann@gmail.com>
2024-09-20 07:34:37 +0200

    update img

--- src/components/CurrentProject.jsx
+++ src/components/CurrentProject.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { motion } from "framer-motion";
+
 
 import image1 from "/src/assets/img/currentproject/images1.png";
+import image2 from "/src/assets/img/currentproject/images2.png";
+
 
 const CurrentProject = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const images = [
     image1,
+    image2,
 
   ];
 
@@ -25,7 +30,11 @@ const CurrentProject = () => {
 
   return (
     <div className="w-full mb-10">
-      <h1 className="text-3xl font-bold text-center mb-6 text-neutral-100">Projet Actuel</h1>
+      <motion.h1 
+              whileInView={{ opacity: 1, y: 0 }}
+              initial={{ opacity: 0, y: -100 }}
+              transition={{ duration: 0.5 }}
+              className="text-3xl font-bold text-center mb-6 text-neutral-100">Projet Actuel</motion.h1>
       <div className="relative w-full h-[50vh] overflow-hidden mb-8">
         <img
           src={images[currentImageIndex]}
@@ -45,12 +54,16 @@ const CurrentProject = () => {
           <FaChevronRight />
         </button>
       </div>
-      <div className="max-w-3xl mx-auto px-4 border-b border-neutral-800 pb-12">
+      <motion.div
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+      className="max-w-3xl mx-auto px-4 border-b border-neutral-800 pb-12">
         <h2 className="text-2xl font-bold mb-4 text-center text-neutral-100">Mon Projet en Cours</h2>
         <p className="text-neutral-300 text-center leading-relaxed">
           Réalisation d'un scraper web afin de récupérer de la data pour potentiellement pouvoir faire un bot d'arbitrage
         </p>
-      </div>
+      </motion.div>
     </div>
   );
 };
